a864ea8a2ff32fb1e71d59aff3bc488159d5f782
kazij317-code <kazij317@gmail.com>
2026-05-20 02:17:39 +0600

    Added add idea page

--- src/lib/ideas/data.js
+++ src/lib/ideas/data.js
@@ -1,12 +1,20 @@
-export const fetchIdeas = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ideas`)
-    const data = await res.json();
-    
-}
+export const fetchIdeas = async (searchTerm = '', category = '') => {
+  
+  // Api Calling for all ideas data
+  let url = `${process.env.NEXT_PUBLIC_API_URL}/ideas?`;
+  const params = new URLSearchParams();
+  if (searchTerm) params.set('search', searchTerm);
+  if (category && category !== 'All') params.set('category', category);
 
+  const res = await fetch(url + params.toString());
+  const data = await res.json();
+  return data || [];
+};
 
+// Api calling for Featured ideas data
 export const fetchFeaturedIdeas = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/featured`);
   const data = await res.json();
-  
+  return data || [];
 };
+
