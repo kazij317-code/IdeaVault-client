bbad4a52dd4a410494ffb4f8d39779edf209b11d
kazij317-code <kazij317@gmail.com>
2026-05-20 09:14:26 +0600

    added not found page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import FeaturedIdeas from '@/components/FeaturedIdeas';
+import Features from '@/components/Features';
 import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
       <FeaturedIdeas />
-      
+
+      <Features />
     </div>
   );
 }
-
